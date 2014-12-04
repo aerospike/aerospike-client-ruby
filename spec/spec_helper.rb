2d0282a1623709e140510234c6fbfd8d8c82fd7b
Manoj Mathai <manoj@dispatchtrack.com>
2014-12-04 20:21:54 +0530

    Require benchmark in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ SimpleCov.start
 $:.unshift((Pathname(__FILE__).dirname.parent + 'lib').to_s)
 
 require 'aerospike'
-
+require 'benchmark'
 # Log to a StringIO instance to make sure no exceptions are rasied by our
 # logging code.
 Aerospike.logger = Logger.new(StringIO.new, Logger::DEBUG)
