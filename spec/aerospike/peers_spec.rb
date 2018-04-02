087c10a86523fd275c52b3711f92eac37f0cb8db
Sebastian Wallin <sebastian.wallin@gmail.com>
2018-04-02 10:25:54 -0700

    Added Peers#reset_refresh_count! method

--- spec/aerospike/peers_spec.rb
+++ spec/aerospike/peers_spec.rb
@@ -17,32 +17,16 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-module Aerospike
-  class Peers
-    attr_accessor :peers, :hosts, :nodes, :refresh_count, :use_peers, :generation_changed
+describe Aerospike::Peers do
+  let(:instance) { described_class.new }
 
-    def initialize
-      @peers = ::Array.new
-      @hosts = ::Set.new
-      @nodes = {}
-      @use_peers = true
-      @refresh_count = 0
+  describe '#reset_refresh_count!' do
+    subject(:reset_refresh_count) { instance.reset_refresh_count! }
+
+    before do
+      instance.refresh_count = 1
     end
 
-    def find_node_by_name(node_name)
-      @nodes[node_name]
-    end
-
-    def generation_changed?
-      @generation_changed == true
-    end
-
-    def reset_refresh_count!
-      @refresh_count = 0
-    end
-
-    def use_peers?
-      @use_peers == true
-    end
+    it { expect { reset_refresh_count }.to change(instance, :refresh_count).from(1).to(0) }
   end
 end
