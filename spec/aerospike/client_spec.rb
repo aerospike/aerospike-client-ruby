fb720a7fc19915c291103c0b8923e5e76fcabb0c
Jan Hecking <jhecking@aerospike.com>
2018-04-05 12:59:59 +0800

    Add more granular specs for batch_get

--- spec/aerospike/client_spec.rb
+++ spec/aerospike/client_spec.rb
@@ -15,11 +15,7 @@
 # the License.
 
 require "spec_helper"
-
-require 'aerospike/host'
-require 'aerospike/key'
-require 'aerospike/bin'
-
+require 'aerospike'
 require 'benchmark'
 
 describe Aerospike::Client do
@@ -543,110 +539,147 @@ describe Aerospike::Client do
 
   context "Batch commands" do
 
-    it "should successfully check existence of many keys" do
-      KEY_CNT = 3000
-      keys = []
-      KEY_CNT.times do |i|
-        keys << key = Support.gen_random_key()
-        client.put(key, Aerospike::Bin.new('bin', 'value')) if i % 2 == 0
+    describe "#batch_exists" do
+      it "should successfully check existence of many keys" do
+        KEY_CNT = 3000
+        keys = []
+        KEY_CNT.times do |i|
+          keys << key = Support.gen_random_key()
+          client.put(key, Aerospike::Bin.new('bin', 'value')) if i % 2 == 0
+        end
+
+        exists = client.batch_exists(keys)
+
+        expect(exists.length).to eq KEY_CNT
+
+        exists.each_with_index do |elem, i|
+          expect(elem).to be (i % 2 == 0)
+        end
       end
 
-      exists = client.batch_exists(keys)
+      it "should successfully check existence of keys" do
+        key1 = Support.gen_random_key
+        key2 = Support.gen_random_key
+        key3 = Support.gen_random_key
 
-      expect(exists.length).to eq KEY_CNT
+        client.put(key1, Aerospike::Bin.new('bin', 'value'))
+        client.put(key3, Aerospike::Bin.new('bin', 'value'))
 
-      exists.each_with_index do |elem, i|
-        expect(elem).to be (i % 2 == 0)
+        exists = client.batch_exists([key1, key2, key3])
+
+        expect(exists.length).to eq 3
+
+        expect(exists[0]).to eq(true)
+        expect(exists[1]).to eq(false)
+        expect(exists[2]).to eq(true)
       end
     end
 
-    it "should successfully check existence of keys" do
-      key1 = Support.gen_random_key
-      key2 = Support.gen_random_key
-      key3 = Support.gen_random_key
+    describe "#batch_get" do
+      shared_examples_for 'a batch_get request' do
+        let(:batch_policy) {
+          Aerospike::BatchPolicy.new(use_batch_direct: use_batch_direct)
+        }
+        let(:existing_keys) { Array.new(3) { Support.gen_random_key } }
+        let(:no_such_key) { Support.gen_random_key }
+        let(:keys) { existing_keys }
+        let(:bins) { nil }
+        subject(:result) { client.batch_get(keys, bins, batch_policy) }
 
-      client.put(key1, Aerospike::Bin.new('bin', 'value'))
-      client.put(key3, Aerospike::Bin.new('bin', 'value'))
+        before do
+          existing_keys.each_with_index do |key, idx|
+            client.put(key, {
+              'idx' => idx,
+              'key' => key.user_key,
+              'rnd' => rand
+            })
+          end
+        end
 
-      exists = client.batch_exists([key1, key2, key3])
+        context 'when fetching existing records' do
+          it 'returns one record per key' do
+            expect(result.length).to be keys.length
+          end
 
-      expect(exists.length).to eq 3
+          it 'returns the records in the same order' do
+            expect(result.map(&:key)).to eql keys
+          end
+        end
 
-      expect(exists[0]).to eq(true)
-      expect(exists[1]).to eq(false)
-      expect(exists[2]).to eq(true)
+        context 'when fetching a non-existent record' do
+          let(:keys) { [no_such_key].concat(existing_keys) }
+
+          it 'returns one item per key' do
+            expect(result.length).to be keys.length
+          end
+
+          it 'returns nil instead of the non-existent record' do
+            expect(result.first).to be_nil
+          end
+
+          it 'returns the remaining records' do
+            expect(result[2..-1].map(&:key)).to eql keys.slice(2..-1)
+          end
+        end
+
+        context 'when no bins are specified' do
+          let(:bins) { nil }
+
+          it 'returns all the bins' do
+            expect(result.first.bins.keys).to eql %w[idx key rnd]
+          end
+        end
+
+        context 'when given a list of bin names' do
+          let(:bins) { %w[ idx rnd ] }
+
+          it 'returns only the specified bins' do
+            expect(result.first.bins.keys).to eql %w[idx rnd]
+          end
+        end
+      end
+
+      context 'using batch index protocol' do
+        let(:use_batch_direct) { false }
+
+        it_behaves_like 'a batch_get request'
+      end
+
+      context 'using batch direct protocol' do
+        let(:use_batch_direct) { true }
+
+        it_behaves_like 'a batch_get request'
+      end
     end
 
-    it "should successfully get keys using batch_index protocol" do
-      key1 = Support.gen_random_key
-      key2 = Support.gen_random_key
-      key3 = Support.gen_random_key
+    describe "#batch_get_header" do
+      it "should successfully get headers for keys" do
+        key1 = Support.gen_random_key
+        key2 = Support.gen_random_key
+        key3 = Support.gen_random_key
 
-      bin = Aerospike::Bin.new('bin', 'value')
-      client.put(key1, bin)
-      client.put(key3, bin)
+        bin = Aerospike::Bin.new('bin', 'value')
+        client.put(key1, bin, ttl: 1000)
+        client.put(key3, bin, ttl: 1000)
 
-      records = client.batch_get([key1, key2, key3], ['bin'])
+        records = client.batch_get_header([key1, key2, key3])
 
-      expect(records.length).to eq 3
+        expect(records.length).to eq 3
 
-      expect(records[0].key).to eq key1
-      expect(records[0].bins).to eq ({bin.name => bin.value})
+        expect(records[0].key).to eq key1
+        expect(records[0].key.user_key).to eq key1.user_key
+        expect(records[0].bins).to be nil
+        expect(records[0].generation).to be 1
+        expect(records[0].ttl).to be_within(10).of(1000)
 
-      expect(records[1]).to be nil
+        expect(records[1]).to be nil
 
-      expect(records[2].key).to eq key3
-      expect(records[2].bins).to eq ({bin.name => bin.value})
-    end
-
-    it "should successfully get keys using batch_direct protocol" do
-      key1 = Support.gen_random_key
-      key2 = Support.gen_random_key
-      key3 = Support.gen_random_key
-
-      bin = Aerospike::Bin.new('bin', 'value')
-      client.put(key1, bin)
-      client.put(key3, bin)
-
-      records = client.batch_get([key1, key2, key3], ['bin'],{:use_batch_direct => true})
-
-      expect(records.length).to eq 3
-
-      expect(records[0].key).to eq key1
-      expect(records[0].bins).to eq ({bin.name => bin.value})
-
-      expect(records[1]).to be nil
-
-      expect(records[2].key).to eq key3
-      expect(records[2].bins).to eq ({bin.name => bin.value})
-    end
-
-    it "should successfully get headers for keys" do
-      key1 = Support.gen_random_key
-      key2 = Support.gen_random_key
-      key3 = Support.gen_random_key
-
-      bin = Aerospike::Bin.new('bin', 'value')
-      client.put(key1, bin, ttl: 1000)
-      client.put(key3, bin, ttl: 1000)
-
-      records = client.batch_get_header([key1, key2, key3])
-
-      expect(records.length).to eq 3
-
-      expect(records[0].key).to eq key1
-      expect(records[0].key.user_key).to eq key1.user_key
-      expect(records[0].bins).to be nil
-      expect(records[0].generation).to be 1
-      expect(records[0].ttl).to be_within(10).of(1000)
-
-      expect(records[1]).to be nil
-
-      expect(records[2].key).to eq key3
-      expect(records[2].key.user_key).to eq key3.user_key
-      expect(records[2].bins).to be nil
-      expect(records[2].generation).to be 1
-      expect(records[2].ttl).to be_within(10).of(1000)
+        expect(records[2].key).to eq key3
+        expect(records[2].key.user_key).to eq key3.user_key
+        expect(records[2].bins).to be nil
+        expect(records[2].generation).to be 1
+        expect(records[2].ttl).to be_within(10).of(1000)
+      end
     end
 
   end
