a804df44fd2e05a9471ca18ae922ebd931426fed
Jan Hecking <jhecking@aerospike.com>
2019-04-02 12:53:59 +0800

    Don't raise KEY_NOT_FOUND error in any queries
    Resolves #77.

--- lib/aerospike/query/stream_command.rb
+++ lib/aerospike/query/stream_command.rb
@@ -36,15 +36,14 @@ module Aerospike
 
         # The only valid server return codes are "ok" and "not found".
         # If other return codes are received, then abort the batch.
-        if result_code != 0
-          # if there is no recordset defined, it means this is an Execute UDF On Query command
-          # return successfully
-          if (@recordset == nil) && (result_code == Aerospike::ResultCode::KEY_NOT_FOUND_ERROR)
-            # consume the rest of the input buffer from the socket
-            read_bytes(receive_size - @data_offset) if @data_offset < receive_size
-
-            return nil
-          end
+        case result_code
+        when Aerospike::ResultCode::OK
+          # noop
+        when Aerospike::ResultCode::KEY_NOT_FOUND_ERROR
+          # consume the rest of the input buffer from the socket
+          read_bytes(receive_size - @data_offset) if @data_offset < receive_size
+          return nil
+        else
           raise Aerospike::Exceptions::Aerospike.new(result_code)
         end
 
